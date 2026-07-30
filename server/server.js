ac687306d293feef9aa324ef0ad58d0c5542757a
omthoriya <omthoriya@gmail.com>
2026-07-30 14:52:09 +0530

    Complete MERN E-Commerce Project

--- server/server.js
+++ server/server.js
@@ -1,35 +1,5 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-
-dotenv.config();
-
-require("./config/db");
-
-const userRoutes = require("./routes/userRoutes");
-const categoryRoutes = require("./routes/categoryRoutes");
-const productRoutes = require("./routes/productRoutes");
-const cartRoutes = require("./routes/cartRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/uploads", express.static("uploads"));
-
-app.get("/", (req, res) => {
-  res.send("E-Commerce API Running...");
-});
-
-app.use("/api/users", userRoutes);
-app.use("/api/categories", categoryRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/admin", adminRoutes);
+require("dotenv").config();
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
