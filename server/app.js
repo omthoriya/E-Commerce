8bc3bb4fdabfa221114da1e04929f4217e1be6e3
omthoriya <omthoriya@gmail.com>
2026-07-30 17:21:09 +0530

    Connect backend to TiDB Cloud

--- server/app.js
+++ server/app.js
@@ -15,7 +15,12 @@ const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
